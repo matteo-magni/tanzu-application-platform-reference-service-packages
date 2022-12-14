68b2d7485a80edc1b0e424aeaf9982c84201e3f2
Matteo Magni <matteo.magni@gmail.com>
2022-12-14 19:06:30 +0100

    test azure psql

---
+++ scripts/carvel-azure-install-aso.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+CERT_MANAGER_MANIFEST="https://github.com/jetstack/cert-manager/releases/download/v1.8.2/cert-manager.yaml"
+ASO_MANIFEST="https://github.com/Azure/azure-service-operator/releases/download/v2.0.0-beta.3/azureserviceoperator_v2.0.0-beta.3.yaml"
+ASO_NAMESPACE="azureserviceoperator-system"
+
+# install certmanager
+kubectl apply -f ${CERT_MANAGER_MANIFEST}
+kubectl -n cert-manager wait --for=condition=Available=True deployments.apps cert-manager
+kubectl -n cert-manager wait --for=condition=Available=True deployments.apps cert-manager-cainjector
+kubectl -n cert-manager wait --for=condition=Available=True deployments.apps cert-manager-webhook
+kubectl wait --for=condition=Available apiservices.apiregistration.k8s.io v1.cert-manager.io
+kubectl wait --for=condition=Available apiservices.apiregistration.k8s.io v1.acme.cert-manager.io
+
+# install ASO
+kubectl apply --server-side=true -f ${ASO_MANIFEST}
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: aso-controller-settings
+  namespace: ${ASO_NAMESPACE}
+stringData:
+  AZURE_SUBSCRIPTION_ID: "$AZURE_SUBSCRIPTION_ID"
+  AZURE_TENANT_ID: "$AZURE_TENANT_ID"
+  AZURE_CLIENT_ID: "$AZURE_CLIENT_ID"
+  AZURE_CLIENT_SECRET: "$AZURE_CLIENT_SECRET"
+EOF
+
+kubectl -n ${ASO_NAMESPACE} wait --for=condition=Available deployments.apps azureserviceoperator-controller-manager
---
+++ scripts/carvel-e2e-azure-psql.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+pushd $(dirname $0)
+
+# install ASO and dependencies
+./carvel-azure-install-aso.sh
+
+# install package
+
+
+# create claim
+
+# run test
+
+# verify results
+
+# clean up
