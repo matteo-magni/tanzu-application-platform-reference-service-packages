25c24e74b9efdf20b46c8f742910b2491cbd32d5
Matteo Magni <matteo.magni@gmail.com>
2022-12-19 10:20:59 +0100

    install package in ci

--- scripts/carvel-e2e-azure-psql.sh
+++ scripts/carvel-e2e-azure-psql.sh
@@ -1,11 +1,49 @@
 #!/usr/bin/env bash
 
-pushd $(dirname $0)
+set -euo pipefail
+
+PACKAGE_NAMESPACE=${PACKAGE_NAMESPACE:-services}
+
+pushd $(dirname $0)/../
 
 # install ASO and dependencies
-./carvel-azure-install-aso.sh
+# ./carvel-azure-install-aso.sh
+
+# prepare values file for package
+# VALUES_FILE=$(mktemp)
+# trap "rm ${VALUES_FILE}" EXIT
+
+NAME="$(dd if=/dev/urandom bs=20 count=1 2>/dev/null | sha1sum | head -c 20)"
+LOCATION="${LOCATION:-westeurope}"
+PUBLIC_IP=$(curl -sSf https://api.ipify.org)
+
+VALUES="$(cat <<EOF
+---
+name: ${NAME}
+namespace: ${PACKAGE_NAMESPACE}
+location: ${LOCATION}
+aso_controller_namespace: azureserviceoperator-system
+create_namespace: false
+
+server:
+  administrator_name: root
+
+database:
+  name: testdb
+
+firewall_rules:
+  - startIpAddress: ${PUBLIC_IP}
+    endIpAddress: ${PUBLIC_IP}
+
+resource_group:
+  use_existing: false
+  name: aso-psql-${NAME}
+EOF
+)"
 
 # install package
+kubectl create namespace ${PACKAGE_NAMESPACE} || true
+ytt -f config/carvel/package-install -v refName="${PACKAGE_METADATA_NAME}" -v namespace=${PACKAGE_NAMESPACE} -v version=${PACKAGE_VERSION} -v values="${VALUES}" | kubectl apply -f -
 
 # create claim
 
@@ -14,3 +52,5 @@ pushd $(dirname $0)
 # verify results
 
 # clean up
+
+popd
